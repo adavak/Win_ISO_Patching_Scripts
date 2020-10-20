63dcfe0e6c549d75c3470d82b2647044de10d77a
Adavak LEE <adavak@adavak.com>
2020-10-21 06:35:05 +0800

    Revert "update October #2"
    This reverts commit 31425e0f385aaf47ea6bd55f51262ab4d812fdb2.

--- Start.cmd
+++ Start.cmd
@@ -54,7 +54,6 @@ for /f "tokens=4 delims=:. " %%# in ('dism.exe /english /get-wiminfo /wimfile:"%
 for /f "tokens=2 delims=: " %%# in ('dism.exe /english /get-wiminfo /wimfile:"%~dp0%ISODir%\sources\install.wim" /index:1 ^| find /i "Architecture"') do set arch=%%#
 
 if %build%==18362 (set /a build=18363)
-if %build%==19041 (set /a build=19042)
 if %build%==14393 if %arch%==x86 (goto :NOT_SUPPORT)
 
 if NOT EXIST %aria2% goto :NO_ARIA2_ERROR
