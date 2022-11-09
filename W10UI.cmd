6ee4ab04bc2742fdccc8938932cd2170d935e65e
Adavak <adavak@adavak.com>
2022-11-09 21:04:46 +0800

    .net framework 4.8.1 x86 add.

--- W10UI.cmd
+++ W10UI.cmd
@@ -354,6 +354,11 @@ if %netfx481%==1 if exist "!repo!\*ndp48_*.*" if exist "!repo!\*ndp481_*.*" if %
   del /f /q "!repo!\*ndp48.*" %_Nul1%
   copy /y "%~dp0bin\windows10.0-kb5011050-x64_02bbcfe63655f135aeb4ead88bcd7d770446b674_net481_zhcn.cab" "!repo!\" %_Nul1%
 )
+if %netfx481%==1 if exist "!repo!\*ndp48_*.*" if exist "!repo!\*ndp481_*.*" if %arch%==x86 (
+  del /f /q "!repo!\*ndp48_*.*" %_Nul1%
+  del /f /q "!repo!\*ndp48.*" %_Nul1%
+  copy /y "%~dp0bin\windows10.0-kb5011050-x86_387b07d30821f250092085c532fd2a2f463e5df3_net481_zhcn.cab" "!repo!\" %_Nul1%
+)
 if %netfx481%==0 if exist "!repo!\*ndp481_*.*" if exist "!repo!\*ndp48_*.*" (
   del /f /q "!repo!\*ndp481_*.*" %_Nul1%
   del /f /q "!repo!\*ndp481.*" %_Nul1%
