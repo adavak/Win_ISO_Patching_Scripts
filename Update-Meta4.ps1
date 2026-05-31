531599853a6f11ee3edff7ce566cc7f282b02b2c
Adavak <adavak@adavak.com>
2026-05-31 23:01:24 +0800

    chore: add SSU: bundled to server output for consistency

--- Update-Meta4.ps1
+++ Update-Meta4.ps1
@@ -586,6 +586,7 @@ foreach ($bn in $Build) {
             $serverFiles = Add-CheckpointCU -OldMeta4 $old -CurrentFiles $serverFiles -BuildNum $bn
             if ($serverFiles -isnot [array]) { $serverFiles = @($serverFiles) }
 
+            Write-Host "  SSU: bundled" -ForegroundColor DarkGray
             Write-Host "  LCU..." -NoNewline
             try {
                 # Always run chain + bootstrap first (like other builds)
